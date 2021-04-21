1606377d438ffbac3e4567736ede48e4fb085b53
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-04-21 22:30:43 +0100

    dark theme done, even when refreshing the app

--- flutter_architecture/lib/app/app_widget.dart
+++ flutter_architecture/lib/app/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './pages/home/home_page.dart';
 import 'controllers/app_controller.dart';
+import './services/shared_local_storage_service.dart';
 
 class App extends StatelessWidget {
   @override
@@ -9,13 +10,12 @@ class App extends StatelessWidget {
       valueListenable: AppController.instance.themeSwitch,
       builder: (context, isDark, child) {
         return MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            brightness: isDark ? Brightness.dark : Brightness.light,
-            primarySwatch: Colors.blue,
-          ),
-          home: HomePage(),
-        );
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              brightness: isDark ? Brightness.dark : Brightness.light,
+              primarySwatch: Colors.blue,
+            ),
+            home: HomePage());
       },
     );
   }
--- flutter_architecture/lib/app/controllers/app_controller.dart
+++ flutter_architecture/lib/app/controllers/app_controller.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/foundation.dart';
+import '../services/shared_local_storage_service.dart';
 import '../models/appConfig_model.dart';
+import '../interfaces/local_storage_interface.dart';
 
 class AppController {
   // now we have our sington protected
   static final AppController instance = AppController._();
-  AppController._();
+  AppController._() {
+    _iLocalStorage.get('isDark').then((val) {
+      themeSwitch.value = val;
+    });
+  }
   final config = AppConfigModel();
   bool get isDark => config.themeSwitch.value;
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
+  ILocalStorage _iLocalStorage = SharedLocalStorageService();
 
-  void changeTheme(bool value) => themeSwitch.value = value;
+  void changeTheme(bool value) async {
+    themeSwitch.value = value;
+    await _iLocalStorage.put('isDark', value);
+  }
 }
